2dbfc8819342bf18a2c4f124b4d9ce7e9e89d2fa
Liv Adamek <157637894+livadamek@users.noreply.github.com>
2024-12-04 11:45:10 -0600

    edit

---
+++ code/README.md
@@ -0,0 +1,15 @@
+# Code
+
+## notebooks
+Python Jupyter notebooks. Files should be prefixed with the order they should un.
+
+- 00_file1.ipynb  
+- 01_file1.ipynb  
+- 02_file1.ipynb  
+
+## scripts
+Python JScripts (.py). Files should be prefixed with the order they should un.
+
+- 00_file1.py  
+- 01_file1.py  
+- 02_file1.py  
--- code/scripts/sample.py
+++ code/scripts/sample.py
@@ -1,1 +1,17 @@
-#place holder for any python scripts (.py)
+# Code
+
+## notebooks
+Python Jupyter notebooks. Files should be prefixed with the order they should un.
+
+- 00_file1.ipynb  
+- 01_file1.ipynb  
+- 02_file1.ipynb  
+
+## notebooks
+Python Jupyter notebooks. Files should be prefixed with the order they should un.
+
+- 00_file1.ipynb  
+- 01_file1.ipynb  
+- 02_file1.ipynb  
+
+
